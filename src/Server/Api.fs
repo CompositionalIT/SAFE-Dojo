9bf4c9391ab3b356ce4351521f0d1511fbb3915f
Isaac Abraham <isaac.abraham@gmail.com>
2018-03-27 13:03:11 +0200

    Postcode validation on all endpoints.

--- src/Server/Api.fs
+++ src/Server/Api.fs
@@ -3,41 +3,46 @@ module Api
 open DataAccess
 open FSharp.Data.UnitSystems.SI.UnitNames
 open Giraffe
+open Microsoft.AspNetCore.Http
 open Saturn
 open Shared
-open Microsoft.AspNetCore.Http
 
 let private london = { Latitude = 51.5074; Longitude = 0.1278 }
+let invalidPostcode next (ctx:HttpContext) =
+    ctx.SetStatusCode 400
+    text "Invalid postcode" next ctx
 
 let getDistanceFromLondon postcode next (ctx:HttpContext) = task {
     if Validation.validatePostcode postcode then
         let! location = getLocation postcode
         let distanceToLondon = getDistanceBetweenPositions location.LatLong london
         return! json { Postcode = postcode; Location = location; DistanceToLondon = (distanceToLondon / 1000.<meter>) } next ctx
-    else
-        ctx.SetStatusCode 400
-        return! text "Invalid postcode" next ctx }
+    else return! invalidPostcode next ctx }
 
 let getCrimeReport postcode next ctx = task {
-    let! location = getLocation postcode
-    let! reports = Crime.getCrimesNearPosition location.LatLong
-    let crimes =
-        reports
-        |> Array.countBy(fun r -> r.category)
-        |> Array.sortByDescending snd
-        |> Array.map(fun (k, c) -> { Crime = k; Incidents = c })
-    return! json crimes next ctx }
+    if Validation.validatePostcode postcode then
+        let! location = getLocation postcode
+        let! reports = Crime.getCrimesNearPosition location.LatLong
+        let crimes =
+            reports
+            |> Array.countBy(fun r -> r.category)
+            |> Array.sortByDescending snd
+            |> Array.map(fun (k, c) -> { Crime = k; Incidents = c })
+        return! json crimes next ctx
+    else return! invalidPostcode next ctx }
 
-let getWeatherForPosition postcode next ctx= task {
-    let! location = getLocation postcode
-    let! weather = Weather.getWeatherForPosition location.LatLong
-    let response =
-        { WeatherResponse.Description =
-            weather.consolidated_weather
-            |> Array.maxBy(fun w -> w.weather_state_name)
-            |> fun w -> w.weather_state_name
-          AverageTemperature = weather.consolidated_weather |> Array.averageBy(fun r -> r.the_temp) }
-    return! json response next ctx }
+let getWeatherForPosition postcode next ctx = task {
+    if Validation.validatePostcode postcode then
+        let! location = getLocation postcode
+        let! weather = Weather.getWeatherForPosition location.LatLong
+        let response =
+            { WeatherResponse.Description =
+                weather.consolidated_weather
+                |> Array.maxBy(fun w -> w.weather_state_name)
+                |> fun w -> w.weather_state_name
+              AverageTemperature = weather.consolidated_weather |> Array.averageBy(fun r -> r.the_temp) }
+        return! json response next ctx
+    else return! invalidPostcode next ctx }
 
 let apiRouter = scope {
     pipe_through (pipeline { set_header "x-pipeline-type" "Api" })
