0d37073edca5de713731377d03d57667a0521fd5
Joost <joost@compositional-it.com>
2024-01-08 11:51:07 +0000

    Update Helpers.fs and make corresponding changes in Build.fs

--- Build.fs
+++ Build.fs
@@ -14,15 +14,15 @@ let deployPath = Path.getFullName "deploy"
 
 Target.create "Clean" (fun _ ->
     Shell.cleanDir deployPath
-    run dotnet "fable clean --yes" clientPath // Delete *.fs.js files created by Fable
+    run dotnet [ "fable"; "clean"; "--yes" ] clientPath // Delete *.fs.js files created by Fable
 )
 
-Target.create "InstallClient" (fun _ -> run npm "install" ".")
+Target.create "InstallClient" (fun _ -> run npm [ "install" ] ".")
 
 Target.create "Bundle" (fun _ ->
     [
-        "server", dotnet $"publish -c Release -o \"{deployPath}\"" serverPath
-        "client", dotnet "fable -o output -s --run npx vite build" clientPath
+        "server", dotnet [ "publish"; "-c"; "Release"; "-o"; "\"{deployPath}\"" ] serverPath
+        "client", dotnet [ "fable"; "-o"; "output"; "-s"; "--run"; "npx"; "vite"; "build" ] clientPath
     ]
     |> runParallel)
 
@@ -40,15 +40,15 @@ Target.create "Azure" (fun _ ->
     deployment |> Deploy.execute "safe-dojo" Deploy.NoParameters |> ignore)
 
 Target.create "Run" (fun _ ->
-    run dotnet "build" sharedPath
+    run dotnet [ "build" ] sharedPath
 
     [
-        "server", dotnet "watch run" serverPath
-        "client", dotnet "fable watch -o output -s --run npx vite" clientPath
+        "server", dotnet [ "watch"; "run" ] serverPath
+        "client", dotnet [ "fable"; "watch"; "-o"; "output"; "-s"; "--run"; "npx"; "vite" ] clientPath
     ]
     |> runParallel)
 
-Target.create "Format" (fun _ -> run dotnet "fantomas . -r" "src")
+Target.create "Format" (fun _ -> run dotnet ["fantomas"; "."; "-r"] "src")
 
 open Fake.Core.TargetOperators
 
--- Helpers.fs
+++ Helpers.fs
@@ -3,36 +3,37 @@ module Helpers
 open Fake.Core
 
 let initializeContext () =
-    let execContext = Context.FakeExecutionContext.Create false "build.fsx" [ ]
+    let execContext = Context.FakeExecutionContext.Create false "build.fsx" []
     Context.setExecutionContext (Context.RuntimeContext.Fake execContext)
 
 module Proc =
     module Parallel =
         open System
 
-        let locker = obj()
+        let locker = obj ()
 
-        let colors =
-            [| ConsoleColor.Blue
-               ConsoleColor.Yellow
-               ConsoleColor.Magenta
-               ConsoleColor.Cyan
-               ConsoleColor.DarkBlue
-               ConsoleColor.DarkYellow
-               ConsoleColor.DarkMagenta
-               ConsoleColor.DarkCyan |]
+        let colors = [|
+            ConsoleColor.Blue
+            ConsoleColor.Yellow
+            ConsoleColor.Magenta
+            ConsoleColor.Cyan
+            ConsoleColor.DarkBlue
+            ConsoleColor.DarkYellow
+            ConsoleColor.DarkMagenta
+            ConsoleColor.DarkCyan
+        |]
 
         let print color (colored: string) (line: string) =
-            lock locker
-                (fun () ->
-                    let currentColor = Console.ForegroundColor
-                    Console.ForegroundColor <- color
-                    Console.Write colored
-                    Console.ForegroundColor <- currentColor
-                    Console.WriteLine line)
+            lock locker (fun () ->
+                let currentColor = Console.ForegroundColor
+                Console.ForegroundColor <- color
+                Console.Write colored
+                Console.ForegroundColor <- currentColor
+                Console.WriteLine line)
 
         let onStdout index name (line: string) =
             let color = colors.[index % colors.Length]
+
             if isNull line then
                 print color $"{name}: --- END ---" ""
             else if String.isNotNullOrEmpty line then
@@ -40,6 +41,7 @@ module Proc =
 
         let onStderr name (line: string) =
             let color = ConsoleColor.Red
+
             if isNull line |> not then
                 print color $"{name}: " line
 
@@ -51,9 +53,7 @@ module Proc =
         let printStarting indexed =
             for (index, (name, c: CreateProcess<_>)) in indexed do
                 let color = colors.[index % colors.Length]
-                let wd =
-                    c.WorkingDirectory
-                    |> Option.defaultValue ""
+                let wd = c.WorkingDirectory |> Option.defaultValue ""
                 let exe = c.Command.Executable
                 let args = c.Command.Arguments.ToStartInfo
                 print color $"{name}: {wd}> {exe} {args}" ""
@@ -62,42 +62,43 @@ module Proc =
             cs
             |> Seq.toArray
             |> Array.indexed
-            |> fun x -> printStarting x; x
+            |> fun x ->
+                printStarting x
+                x
             |> Array.map redirect
             |> Array.Parallel.map Proc.run
 
-let createProcess exe arg dir =
-    CreateProcess.fromRawCommandLine exe arg
+let createProcess exe args dir =
+    // Use `fromRawCommand` rather than `fromRawCommandLine`, as its behaviour is less likely to be misunderstood.
+    // See https://github.com/SAFE-Stack/SAFE-template/issues/551.
+    CreateProcess.fromRawCommand exe args
     |> CreateProcess.withWorkingDirectory dir
     |> CreateProcess.ensureExitCode
 
-let dotnet = createProcess "dotnet"
-let npm =
+let dotnet args dir = createProcess "dotnet" args dir
+
+let npm args dir =
     let npmPath =
         match ProcessUtils.tryFindFileOnPath "npm" with
         | Some path -> path
         | None ->
-            "npm was not found in path. Please install it and make sure it's available from your path. " +
-            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+            "npm was not found in path. Please install it and make sure it's available from your path. "
+            + "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
             |> failwith
 
-    createProcess npmPath
+    createProcess npmPath args dir
 
-let run proc arg dir =
-    proc arg dir
-    |> Proc.run
-    |> ignore
+let run proc arg dir = proc arg dir |> Proc.run |> ignore
 
 let runParallel processes =
-    processes
-    |> Proc.Parallel.run
-    |> ignore
+    processes |> Proc.Parallel.run |> ignore
 
 let runOrDefault args =
     try
         match args with
         | [| target |] -> Target.runOrDefault target
         | _ -> Target.runOrDefault "Run"
+
         0
     with e ->
         printfn "%A" e
